e5f6665e579bb92c0fbefd641da06f57261231d4
JunchengLiamLi <juncheng.liam.li@gmail.com>
2022-04-11 20:23:29 +0100

    QC_relaxation

--- MP_power_dispatch/format_data.jl
+++ MP_power_dispatch/format_data.jl
@@ -8,7 +8,8 @@ struct power_system_data
     bus_imag_loads::Vector{Float64}
     bus_volt_mag_max::Vector{Float64}
     bus_volt_mag_min::Vector{Float64}
-
+    ref_bus::Int64
+    
     branches::Vector{Tuple{Int64,Int64,Int64}}
     tap_ratio::Dict{Int64,Float64}
     branch_shunt_suscep::Dict{Int64,Float64}
@@ -24,6 +25,7 @@ struct power_system_data
     gen_pmin::Vector{Float64}
     gen_qmax::Vector{Float64}
     gen_qmin::Vector{Float64}
+    gen_c0::Vector{Float64}
     gen_c1::Vector{Float64}
     gen_c2::Vector{Float64}
 
@@ -40,10 +42,11 @@ function input_matpower_data(bus_data, branch_data, gen_data, gencost_data, base
     branch_resist = Dict{Int64,Float64}()
     branch_react = Dict{Int64,Float64}()
     branch_current_limit = Dict{Int64,Float64}()
-
+    
+    read_tap_ratio(x) = abs(x) < 0.0001 ? 1 : x
     for i in 1:num_branch
         branches[i] = (i,branch_data.fbus[i], branch_data.tbus[i])
-        tap_ratio[i] = 1
+        tap_ratio[i] = read_tap_ratio(branch_data.ratio[i])
         branch_shunt_suscep[i] = branch_data.b[i]
         branch_shunt_conduc[i] = 0.0
         phase_angle[i] = branch_data.angle[i]
@@ -53,8 +56,9 @@ function input_matpower_data(bus_data, branch_data, gen_data, gencost_data, base
     end
     num_gen = length(gen_data.bus)
     gen = 1:num_gen
-    network_data = power_system_data(bus_data.bus_i,bus_data.Bs,bus_data.Gs,bus_data.Pd,bus_data.Qd,bus_data.Vmax, bus_data.Vmin,
+    ref_bus = findfirst(x->x==3, bus_data[!,:type])
+    network_data = power_system_data(bus_data.bus_i,bus_data.Bs,bus_data.Gs,bus_data.Pd,bus_data.Qd,bus_data.Vmax, bus_data.Vmin,ref_bus,
                             branches,tap_ratio,branch_shunt_suscep,branch_shunt_conduc,phase_angle,branch_resist,branch_react,branch_current_limit,
-                            gen,gen_data.bus,gen_data.Pmax,gen_data.Pmin, gen_data.Qmax, gen_data.Qmin, gencost_data.c1, gencost_data.c2, baseMVA)
+                            gen,gen_data.bus,gen_data.Pmax,gen_data.Pmin, gen_data.Qmax, gen_data.Qmin, gencost_data.c0,gencost_data.c1,gencost_data.c2,baseMVA)
     return network_data
 end
